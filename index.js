50103fe02ac62d33968b2e421281f1d176e81aa1
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-27 11:11:35 +0200

    Create a dispatch function for the math operations

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
 const roundedToFixed = function(input, digits){
   var rounder = Math.pow(10, digits);
   return +(Math.round(input * rounder) / rounder).toFixed(digits);
@@ -17,4 +21,23 @@ const multiply = function(a, b) {
 
 const divide = function(a, b) {
   return roundedToFixed(a / b, 10);
+}
+
+const operate = function(operator, firstNumber, secondNumber) {
+  switch(operator) {
+    case '+':
+      return add(firstNumber, secondNumber);
+      break;
+    case '-':
+      return subtract(firstNumber, secondNumber);
+      break;
+    case '*':
+      return multiply(firstNumber, secondNumber);
+      break;
+    case '/':
+      return divide(firstNumber, secondNumber);
+      break;
+    default:
+      return  `Oops! Operator ${operator} not defined`;
+  }
 }
