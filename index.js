9b349138b3eb59cb926b5338066551a053a48d70
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-06-01 14:56:48 +0200

    Add spagetti code to make calculator work

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const RESET_VALUE = 'AC'
 const DISPLAY_INITIAL_VALUE = 0;
+const OPERATORS = ['+','-', '*','/', '='];
 let firstNumber = null;
 let secondNumber = null;
 let operator = null;
 let currentNumber = null;
+let lastOperator = null;
 
-const roundDecimal = function(input, digits){
+const roundDecimal = function(input, digits) {
   var rounder = Math.pow(10, digits);
   return +(Math.round(input * rounder) / rounder).toFixed(digits);
 }
@@ -43,17 +45,80 @@ const operate = function(operator, firstNumber, secondNumber) {
     default:
       return  `Oops! Operator ${operator} not defined`;
   }
-}
+};
 
 const resetCalculator = function() {
   firstNumber = null;
   secondNumber = null;
   currentNumber = null;
   operator = null;
-}
+  updateDisplayLast();
+  updateDisplay(0)
+};
 
 const updateDisplay = function(value) {
   document.querySelector('.display').textContent = value;
+};
+
+const updateDisplayLast = function(firstNumber, secondNumber, operator, result) {
+  if(firstNumber != null && secondNumber != null && operator != null && result != null) {
+    document.querySelector('.display-last').textContent = `${firstNumber} ${operator} ${secondNumber} = ${result}`;
+  } else {
+    document.querySelector('.display-last').textContent = "";
+  }
+  
+};
+
+const isOperator = function(value) {
+  return OPERATORS.includes(value);
+};
+
+const canCalculateResult = function() {
+  return firstNumber !== null && secondNumber !== null && operator !== null;
+};
+
+const handleOperator = function(typedValue) {
+  let result = null;
+
+  if(firstNumber === null) {
+    firstNumber = currentNumber;
+  } else {
+    secondNumber = currentNumber;
+  }
+  console.log('1 first',firstNumber,'operator ',operator, 'second', secondNumber,'canCalculate', canCalculateResult());
+
+  if(canCalculateResult()) {
+    result = operate(operator, firstNumber, secondNumber);
+    if(result > 9999999999) {
+      result = Infinity;
+      resetCalculator();
+    } else {
+      updateDisplay(result);
+      updateDisplayLast(firstNumber, secondNumber, operator, result);
+      firstNumber = result;
+    }
+  }
+
+  console.log('2 first',firstNumber,'operator ',operator, 'second', secondNumber);
+  currentNumber = null;
+  lastOperator = typedValue;
+  if(typedValue !== '=') {
+    operator = typedValue;
+  }
+}
+
+const handleNumber = function(typedValue) { 
+  let displayElem = document.querySelector('.display');
+  let displayValue = currentNumber === null ? 0 : displayElem.textContent;
+  if(lastOperator === '=') {
+    resetCalculator();
+    lastOperator = null;
+  }
+  if(displayValue.length === 10) return;
+  displayValue = +displayValue;
+  currentNumber = displayValue * 10 + +typedValue
+  displayValue = currentNumber;
+  updateDisplay(displayValue);
 }
 
 let controlElem = document.querySelector('.control');
@@ -69,21 +134,16 @@ controlElem.addEventListener('click', function(e) {
     return;
   }
 
-  let displayElem = document.querySelector('.display');
-  let displayValue = currentNumber === null ? 0 : displayElem.textContent;
-
-  if(displayValue.length === 10) {
-    displayValue = Infinity;
-    updateDisplay(displayValue);
-    resetCalculator();
-    return;
-  }
+  // let displayElem = document.querySelector('.display');
+  // let displayValue = currentNumber === null ? 0 : displayElem.textContent;
 
   if(!Number.isNaN(+typedValue)) {
-    displayValue = +displayValue;
-    currentNumber = displayValue * 10 + +typedValue
-    displayValue = currentNumber;
-    updateDisplay(displayValue);
+    handleNumber(typedValue)
+  }
+  console.log('current',currentNumber,'typed val ',typedValue, 'isOperator',isOperator(typedValue))
+ 
+  if(isOperator(typedValue)) {
+    handleOperator(typedValue);
   }
 });
 
